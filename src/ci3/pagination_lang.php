2b4f9a1a05f2a4de55f45b6e4f4cbf8c9e712c6e
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:24:24 +0100

    Squashing Commit!

---
+++ src/ci3/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['pagination_first_link'] = '&lsaquo; प्रथम';
+$lang['pagination_last_link'] = 'आखरी &rsaquo;';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_summary_text'] = '%d से %d तक कुल %d प्रविष्टियाँ दिखा रहा है';
---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - Hindi
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'हिन्दी',
+	'name_en'    => 'Hindi',
+	'locale'     => 'hi_IN',
+	'tag'        => 'hi-IN',
+	'direction'  => 'ltr',
+	'code'       => 'hi',
+	'flag'       => 'in',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'अरबी',
+		'armenian'   => 'अर्मेनियाई',
+		'bengali'    => 'बंगाली',
+		'brazilian'  => 'पुर्तगाली (ब्राजील)',
+		'bulgarian'  => 'बुल्गारियाई',
+		'catalan'    => 'कैटेलान',
+		'chinese'    => 'चीनी (सरलीकृत)',
+		'croatian'   => 'क्रोएशियाई',
+		'czech'      => 'चेक',
+		'danish'     => 'डैनिश',
+		'dutch'      => 'डच',
+		'english'    => 'अंग्रेज़ी',
+		'estonian'   => 'एस्टोनियाई',
+		'filipino'   => 'फिलिपीनो',
+		'finnish'    => 'फिनलैंड',
+		'french'     => 'फ्रेंच',
+		'georgian'   => 'जॉर्जियाई',
+		'german'     => 'जर्मन',
+		'greek'      => 'यूनानी',
+		'hindi'      => 'हिन्दी',
+		'hungarian'  => 'हंगेरियन',
+		'indonesian' => 'इंडोनेशियाई',
+		'italian'    => 'इतालवी',
+		'japanese'   => 'जापानी',
+		'korean'     => 'कोरियाई',
+		'latvian'    => 'लातवियाई',
+		'lithuanian' => 'लिथुआनियाई',
+		'malay'      => 'मलायी',
+		'norwegian'  => 'नॉर्वेजियन',
+		'persian'    => 'फारसी',
+		'polish'     => 'पोलिश',
+		'portuguese' => 'पुर्तगाली',
+		'romanian'   => 'रोमानियाई',
+		'russian'    => 'रूसी',
+		'serbian'    => 'सर्बियाई',
+		'slovak'     => 'स्लोवाक',
+		'slovenian'  => 'स्लोवेनियाई',
+		'spanish'    => 'स्पैनिश',
+		'swahili'    => 'स्वाहिली',
+		'swedish'    => 'स्वीडिश',
+		'taiwanese'  => 'चीनी (पारंपरिक)',
+		'tamil'      => 'तमिल',
+		'thai'       => 'थाई',
+		'turkish'    => 'तुर्की',
+		'ukrainian'  => 'यूक्रेनी',
+		'urdu'       => 'उर्दू',
+		'vietnamese' => 'वियतनामी',
+	],
+];
